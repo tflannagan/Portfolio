01836a92a3bd3e041d3398fbf3f1cb37004e5913
Ty Flannagan <tylerflannagan@yahoo.com>
2022-11-06 20:24:06 -0800

    added cursor functionality

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+
+
 (function () {
   [...document.querySelectorAll(".control")].forEach(button => {
       button.addEventListener("click", function() {
@@ -11,3 +13,13 @@
       document.body.classList.toggle("light-mode");
   })
 })();
+
+var target = document.getElementById('target');
+var targetBox = document.getElementById('target-box');
+
+targetBox.addEventListener('click', mouseClicked);
+
+function mouseClicked (event){
+  var xposition = (event.clientX - target.offsetLeft - target.offsetWidth/2);
+  var yposition = (event.clientY - target.offsetTop - target.offsetHeight/2);
+  target.style.transform = "translate("+ xposition + "px," + yposition + "px)";}

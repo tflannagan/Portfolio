0901f9c01f2c1f596bab79372334a36221b0d1eb
Ty Flannagan <tylerflannagan@yahoo.com>
2022-12-05 22:21:58 -0800

    changed color scheme and updated theme

--- app.js
+++ app.js
@@ -14,13 +14,13 @@
   })
 })();
 
-var target = document.getElementById('target');
-var targetBox = document.getElementById('target-box');
+// var target = document.getElementById('target');
+// var targetBox = document.getElementById('target-box');
 
-targetBox.addEventListener('click', mouseClicked);
+// targetBox.addEventListener('click', mouseClicked);
 
-function mouseClicked (event){
-  var xposition = (event.clientX - target.offsetLeft - target.offsetWidth/2);
-  var yposition = (event.clientY - target.offsetTop - target.offsetHeight/2);
-  target.style.transform = "translate("+ xposition + "px," + yposition + "px)";}
+// function mouseClicked (event){
+//   var xposition = (event.clientX - target.offsetLeft - target.offsetWidth/2);
+//   var yposition = (event.clientY - target.offsetTop - target.offsetHeight/2);
+//   target.style.transform = "translate("+ xposition + "px," + yposition + "px)";}
 
